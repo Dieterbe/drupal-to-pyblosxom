41468083a4b9b22ef87aff3edb2b2cd305660e30
Dieter Plaetinck <dieter@plaetinck.be>
2010-12-26 13:11:38 +0100

    export comments, more elaborate/failsafe reporting at end

--- d2pb.py
+++ d2pb.py
@@ -28,7 +28,18 @@ def start():
 		os._exit(2)
 	return cursor, options
 
+def nodecodename (url_alias, title):
+	if url_alias:
+		# The url alias is usually a pretty clean starting point (at least i do my Url_aliases_like_this_and_dont_use_special_chars)
+		codename = url_alias.lower()
+	else:
+		# first make the string lowercase and replace all useful special chars to something more appropriate:
+		codename = title.lower().replace(' ','_').replace ('&','and')
+		# TODO: there could still be special characters we don't want ($, ^, ...), just remove them. (accented characters should probably become non-accented) (I don't need this in my case)
+	return codename
+
 def main(cursor, options):
+	print "Processing nodes..."
 	try:
 		# I don't have multiple revisions per node in drupal
 		# I don't think pyblosxom can have different teaser per item, so we only use the full body
@@ -48,17 +59,11 @@ def main(cursor, options):
 		sys.stderr.write("Could not execute query: %s:\n%s\n" % (query, str(e)))
 	for row in cursor:
 		print "Processing entry: %s" % row[2]
-		if row[1]:
-			# The url alias is usually a pretty clean starting point (at least i do my Url_aliases_like_this_and_dont_use_special_chars)
-			filename = row[1].lower()
-		else:
-			# first make the string lowercase and replace all useful special chars to something more appropriate:
-			filename = row[2].lower().replace(' ','_').replace ('&','and')
-			# TODO: there could still be special characters we don't want ($, ^, ...), just remove them. (accented characters should probably become non-accented) (I don't need this in my case)
+		codename = nodecodename (row[1], row[2])
 		if row[8] == 'blog':
-			file_path = os.path.join(options.blog,'entries',filename+'.txt')
+			file_path = os.path.join(options.blog,'entries',codename+'.txt')
 		else:
-			file_path = os.path.join(options.blog,'static',filename+'.txt')
+			file_path = os.path.join(options.blog,'static',codename+'.txt')
 		if not row[7]:
 			file_path += '.unpublished'
 		try:
@@ -83,8 +88,86 @@ def main(cursor, options):
 		except Exception, e:
 			sys.stderr.write ("Cannot write to %s:\n%s\n" % (file_path,str(e)))
 			os._exit(2)
+	print "Processing comments..."
+	import re
+	try:
+		os.mkdir(os.path.join(options.blog,'entries', 'comments'))
+	except OSError, e:
+		pass
+
+	def replace_words(text, wordmap):
+		for key in wordmap.keys():
+			wordmap['$'+key] = wordmap[key]
+			del wordmap[key]
+		rc = re.compile('|'.join(map(re.escape, wordmap)))
+		def translate(match):
+			return wordmap[match.group(0)]
+		return rc.sub(translate, text)
+	try:
+		f = open('pbcomment.cmt', 'r')
+		comment_tpl = f.read()
+		f.close()
+	except Exception, e:
+		sys.stderr.write("Could not load template comment file\n")
+	try:
+		# status 0 = published, 1 = not published, anything else: never seen
+		# all my comments are in format 1
+		# we assume there are no multiple comments for the same node on the same second (~same filename)
+		# we also assume that you only want to keep published comments, unpublished comments are ignored
+		query = "SELECT url_alias.dst, node.title, comments.cid, comments.pid, \
+			 comments.nid, comments.uid, comments.subject, comments.comment, \
+			 comments.hostname, comments.timestamp, comments.status, comments.name, \
+			 comments.mail, comments.homepage \
+			 FROM comments \
+			 JOIN node on node.nid = comments.nid \
+			 LEFT JOIN url_alias ON url_alias.src = CONCAT ('node/', node.nid) \
+			 ORDER BY comments.nid ASC, comments.timestamp ASC"
+		cursor.execute(query)
+	except Exception, e:
+		sys.stderr.write("Could not execute query: %s:\n%s\n" % (query, str(e)))
+	namecache = {} # used to lookup filenames of earlier comments (useful for threaded comments)
+	from xml.sax.saxutils import escape
+	for row in cursor:
+		subject = row[6].replace("\n", ' ') # not sure why, but my drupal comment subjects allowed newlines, it seems.
+		commentstr = "%s:%s" % (row[1], subject)
+		if row[10] >1:
+			print "WARNING: Unrecognized comment status %i for comment %s" % (row[10], commentstr)
+		if row[10] >0:
+			continue
+		print "Processing entry: %s" % commentstr
+		node = nodecodename (row[0], row[1])
+		comment = {}
+		#comment['title'] = row[1]} # this is comments.py default behavior. but why? why store the title of the node again in the comment (and not show it..)
+		comment['title'] = subject # this is what I do; it doesn't get shown usually, but at least it doesn't get lost. afaik it's the best i can do
+		comment['parent'] = node # this is comments.py default behavior, but this field never seems to be used. and there is no support for anything else anyway
+		# so, for the case comments.py will ever support threaded comments, this should help (key will always be set due to sorting unless db incosistency by drupal)
+		if row[3]:
+			comment['parent'] = namecache[row[3]]
+		# all my comments are in input format 1.  since i wrote the code for input format 3 (for nodes), i won't bother to do it for format 1
+		# the difference is that 3 allows html tags, whereas 1 is just a select set of tags (<a> <em> <strong> <cite> <code> <ul> <ol> <li> <dl> <dt> <dd>)
+		# so you'll need to review comments manually to see if any inappropriate tags need to be left out.
+		# but if you had some quality control in drupal (i.e. no spammers), i doubt this will be much work
+		p = subprocess.Popen(['./text_filter.php'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+		commenthtml = p.communicate(row[7])[0]
+		comment['comment'] = escape(commenthtml)
+		comment['author'] = row[11]
+		comment['author_url'] = row[13]
+		comment['author_email'] = row[12]
+		comment['ip'] = row[8]
+		comment['date'] = time.strftime('%a %d %b %Y', time.gmtime(row[9])) # GMT date, something like: Fri 24 Dec 2010
+		comment['w3cdate'] = time.strftime ('%Y-%m-%dT%H:%M:%SZ', time.gmtime(row[9])) # GMT datetime like this: 2010-12-24T16:09:39Z
+		comment['pubdate'] = "%.2f" % row[9] # unix timestamp (GMT) with centi-second resolution, like 1293206979.53 (drupal is second-resolution)
+		filename = '%s-%s.cmt' % (node, comment['pubdate'])
+		namecache[row[2]] = filename
+
+		comment_str = replace_words(comment_tpl, comment)
+		file_path = os.path.join(options.blog,'entries','comments',filename)
+		fout = open(file_path, 'w')
+		fout.write(comment_str)
+		fout.close()
+
 def report(cursor, options):
-	print "=== Report =="
+	print "\n=== Report ==="
 	print "Please check if these numbers make sense!"
 
 	print "== Drupal =="
@@ -98,34 +181,53 @@ def report(cursor, options):
 	pageunpub = cursor.fetchone()[0]
 	cursor.execute("SELECT COUNT(*) FROM node")
 	total = cursor.fetchone()[0]
+	cursor.execute("SELECT COUNT(*) FROM comments WHERE status = 1")
+	commentsunpub = cursor.fetchone()[0]
+	cursor.execute("SELECT COUNT(*) FROM comments WHERE status = 0")
+	commentspub = cursor.fetchone()[0]
 	cursor.execute("SELECT COUNT(*) FROM comments")
-	comments = cursor.fetchone()[0]
+	comments_all = cursor.fetchone()[0]
 
 	print "nodes published blog: %i"   % pub
 	print "nodes unpublished blog: %i" % unpub
 	print "nodes published page: %i"   % pagepub
 	print "nodes unpublished page: %i" % pageunpub
 	print "nodes all: %i" % total
+	print "comments published: %i" % commentspub
+	print "comments unpublished: %i" % commentsunpub
+	print "comments all: %i" % comments_all
 	total_ours = pub + unpub + pagepub + pageunpub
 	if total != total_ours:
-		print "WARNING: total amount of nodes (%i) doesn't match our number of recognized entries (%i)."
-		print "This means you have a nodes of a status other then 0/1 or type other then blog/page, which this script does not support"
-	print "comments: %i" % comments
+		print "WARNING: total amount of nodes (%i) doesn't match our number of recognized entries (%i)." % (total, total_ours)
+		print "This means you have nodes of a status other then 0/1 or type other then blog/page, which this script does not support"
+	if commentspub + commentsunpub != comments_all:
+		print "WARNING: total amount of comments (%i) doesn't match our number of recognized comments (%i)." % (comments_all, commentspub + commentsunpub)
+		print "This means you have comments of a status other then 0/1, which this script does not support"
 
 	print "== Pyblosxom =="
 	import glob
-	pub         = len(glob.glob1(os.path.join(options.blog,'entries'),"*.txt"))
-	unpub       = len(glob.glob1(os.path.join(options.blog,'entries'),"*.txt.unpublished"))
-	staticpub   = len(glob.glob1(os.path.join(options.blog,'static'),"*.txt"))
-	staticunpub = len(glob.glob1(os.path.join(options.blog,'static'),"*.txt.unpublished"))
-	print "entries published: %i"   % pub
-	print "entries unpublished: %i" % unpub
-	print "statics published: %i"   % staticpub
-	print "statics unpublished: %i" % staticunpub
-	print "entries+statics all: %i" % (pub + unpub + staticpub + staticunpub)
-	print "comments: TODO"
+	pbpub         = len(glob.glob1(os.path.join(options.blog,'entries'),"*.txt"))
+	pbunpub       = len(glob.glob1(os.path.join(options.blog,'entries'),"*.txt.unpublished"))
+	pbstaticpub   = len(glob.glob1(os.path.join(options.blog,'static'),"*.txt"))
+	pbstaticunpub = len(glob.glob1(os.path.join(options.blog,'static'),"*.txt.unpublished"))
+	pbcomments    = len(glob.glob1(os.path.join(options.blog,'entries', 'comments'),"*.cmt"))
+	print "entries published: %i"   % pbpub
+	print "entries unpublished: %i" % pbunpub
+	print "statics published: %i"   % pbstaticpub
+	print "statics unpublished: %i" % pbstaticunpub
+	print "entries+statics all: %i" % (pbpub + pbunpub + pbstaticpub + pbstaticunpub)
+	print "comments: %i"            % pbcomments
+	if pbcomments != commentspub:
+		print "WARNING: pb export misses some comments"
+	if pbpub != pub:
+		print "WARNING: pb export misses some published blog entries"
+	if pbunpub != unpub:
+		print "WARNING: pb export misses some unpublished blog entries"
+	if pbstaticpub != pagepub:
+		print "WARNING: pb export misses some published static pages"
+	if pbstaticunpub != pageunpub:
+		print "WARNING: pb export misses some unpublished static pages"
 
-# comments : comments.cid, comments.nid, comments.uid, comments.subject, comments.comment, comments.timestamp 	status 	format 	thread 	users 	name 	mail 	homepage
 
 if __name__ == '__main__':
 	try:
